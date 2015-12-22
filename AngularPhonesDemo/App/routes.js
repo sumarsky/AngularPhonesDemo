f9b2c6ac9d9c963a273f22e5d88804999c30dea3
Igor Dimchevski <igor.dimchevski@endava.com>
2015-12-22 10:23:26 +0100

    fixed navigation

--- AngularPhonesDemo/App/routes.js
+++ AngularPhonesDemo/App/routes.js
@@ -1,21 +1,42 @@
 ﻿(function (app) {
     "use strict";
+    
+    // Collect the routes
+    app.constant("routes", getRoutes());
 
-    var routes = [
-        { url: "/", settings: { templateUrl: "App/home.html" } },
-        { url: "/phones", settings: { templateUrl: "App/phones/views/phone-list.html" } },
-        { url: "/phones/:id", settings: { templateUrl: "App/phones/views/phone-detail.html" } }
-    ];
+    // Configure the routes and route resolvers
+    app.config(["$routeProvider", "$locationProvider", "routes", routeConfigurator]);
 
-    var registerRoutes = function ($routeProvider, $locationProvider) {
-        routes.forEach(function (route) {
-            $routeProvider.when(route.url, route.settings);
+    function routeConfigurator($routeProvider, $locationProvider, routes) {
+
+        routes.forEach(function (r) {
+            $routeProvider.when(r.url, r.config);
         });
-
         $routeProvider.otherwise({ redirectTo: "/" });
 
         $locationProvider.hashPrefix("!").html5Mode(true);
-    };
-
-    app.config(registerRoutes);
+    }
+    // Define the routes 
+    function getRoutes() {
+        return [
+            {
+                url: "/",
+                config: {
+                    templateUrl: "App/home.html"
+                }
+            },
+            {
+                url: "/phones",
+                config: {
+                    templateUrl: "App/phones/views/phone-list.html"
+                }
+            },
+            {
+                url: "/phones/:id",
+                config: {
+                    templateUrl: "App/phones/views/phone-detail.html"
+                }
+            }
+        ];
+    }
 }(angular.module("app")));
